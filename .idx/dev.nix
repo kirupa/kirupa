62e00dc20ec7e8ba7e4c17517fbf32b3b946a8bb
kirupa <kirupa@google.com>
2024-05-26 18:06:01 +0000

    Fixes to latest version of IDX

--- .idx/dev.nix
+++ .idx/dev.nix
@@ -1,22 +1,30 @@
+# To learn more about how to use Nix to configure your environment
+# see: https://developers.google.com/idx/guides/customize-idx-env
 { pkgs, ... }: {
-  channel = "stable-23.05"; # "stable-23.05" or "unstable"
+  # Which nixpkgs channel to use.
+  channel = "stable-23.11"; # or "unstable"
+  # Use https://search.nixos.org/packages to find packages
   packages = [
-    pkgs.nodejs
+    pkgs.nodejs_20
     pkgs.nodePackages.firebase-tools
+    pkgs.python3
   ];
-  idx.extensions = [];
-  idx.previews = {
-    enable = true;
-    previews = [
-      {
-        command = ["python3" "-m" "http.server" "$PORT" "--bind" "0.0.0.0"];
-        manager = "web";
-        id = "web";
-      }
-      {
-        id = "ios";
-        manager = "ios";
-      }
+  # Sets environment variables in the workspace
+  env = {};
+  idx = {
+    # Search for the extensions you want on https://open-vsx.org/ and use "publisher.id"
+    extensions = [
+      # "vscodevim.vim"
     ];
+    # Enable previews and customize configuration
+    previews = {
+      enable = true;
+      previews = {
+        web = {
+          command = ["python3" "-m" "http.server" "$PORT" "--bind" "0.0.0.0"];
+          manager = "web";
+        };
+      };
+    };
   };
 }
